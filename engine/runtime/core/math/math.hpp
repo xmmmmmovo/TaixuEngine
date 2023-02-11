72cd217bb23d48e41c7c860ecc65b529e1a5c18e
xmmmmmovo <13256172360@163.com>
2023-02-11 02:30:35 +0000

    Add some functions

---
+++ engine/runtime/core/math/math.hpp
@@ -0,0 +1,12 @@
+#ifndef ENGINE_RUNTIME_CORE_MATH_MATH
+#define ENGINE_RUNTIME_CORE_MATH_MATH
+
+#include <limits>
+
+namespace taixu::core {
+
+static const float epsilonFloat = std::numeric_limits<float>::epsilon();
+
+}
+
+#endif /* ENGINE_RUNTIME_CORE_MATH_MATH */
