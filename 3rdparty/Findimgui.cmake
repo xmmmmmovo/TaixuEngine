1da26e13ff99daecdba2d7930c1c6d2eab40913e
xmmmmmovo <13256172360@163.com>
2023-02-13 12:14:10 +0000

    紧急修复：imgui docking 分支

--- 3rdparty/Findimgui.cmake
+++ 3rdparty/Findimgui.cmake
@@ -6,14 +6,14 @@ CPMAddPackage(
     DOWNLOAD_ONLY YES
 )
 
-if(imgui_ADDED)
+if (imgui_ADDED)
     file(GLOB imgui_sources CONFIGURE_DEPENDS ${imgui_SOURCE_DIR}/*.cpp)
     file(GLOB imgui_impl CONFIGURE_DEPENDS
-        ${imgui_SOURCE_DIR}/backends/imgui_impl_glfw.cpp
-        ${imgui_SOURCE_DIR}/backends/imgui_impl_glfw.h
-        ${imgui_SOURCE_DIR}/backends/imgui_impl_opengl3.cpp
-        ${imgui_SOURCE_DIR}/backends/imgui_impl_opengl3.h
-        ${imgui_SOURCE_DIR}/backends/imgui_impl_opengl3_loader.h)
+            ${imgui_SOURCE_DIR}/backends/imgui_impl_glfw.cpp
+            ${imgui_SOURCE_DIR}/backends/imgui_impl_glfw.h
+            ${imgui_SOURCE_DIR}/backends/imgui_impl_opengl3.cpp
+            ${imgui_SOURCE_DIR}/backends/imgui_impl_opengl3.h
+            ${imgui_SOURCE_DIR}/backends/imgui_impl_opengl3_loader.h)
 
     add_library(imgui STATIC ${imgui_sources} ${imgui_impl})
 
@@ -23,4 +23,4 @@ if(imgui_ADDED)
 
     set_target_properties(imgui PROPERTIES LINKER_LANGUAGE CXX)
     set_target_properties(imgui PROPERTIES FOLDER 3rdparty)
-endif()
+endif ()
