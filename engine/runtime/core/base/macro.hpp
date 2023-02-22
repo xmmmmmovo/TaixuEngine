bc2dd56993cf1180c3758a53dc88f1d376e1a785
xinxu Ma <xinxu.ma@student.manchester.ac.uk>
2023-02-22 23:42:48 +0000

    Define enum class for asset type

--- engine/runtime/core/base/macro.hpp
+++ engine/runtime/core/base/macro.hpp
@@ -7,6 +7,16 @@ namespace taixu {
 constexpr char         OPENGL_VERSION[]     = "#version 410";
 constexpr std::int32_t OPENGL_MAJOR_VERSION = 4;
 constexpr std::int32_t OPENGL_MINOR_VERSION = 1;
+
+/* 
+    Define assets types  
+*/
+enum class Assets_Type {
+    MODEL,
+    TEXTURE
+};
+
 }// namespace taixu::core
+
 
 #endif /* ENGINE_RUNTIME_CORE_BASE_MACRO */
