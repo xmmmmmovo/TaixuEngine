1cab004061fc1d2361d82eea5065624f061349e7
Yizhou Song <songyizhou123@gmail.com>
2023-02-24 15:00:22 +0000

    新圩一个人去吃烤肉不带我

--- engine/runtime/core/base/macro.hpp
+++ engine/runtime/core/base/macro.hpp
@@ -4,9 +4,16 @@
 #include <string_view>
 
 namespace taixu {
-constexpr char         OPENGL_VERSION[]     = "#version 410";
-constexpr std::int32_t OPENGL_MAJOR_VERSION = 4;
-constexpr std::int32_t OPENGL_MINOR_VERSION = 1;
+	constexpr char         OPENGL_VERSION[] = "#version 410";
+	constexpr std::int32_t OPENGL_MAJOR_VERSION = 4;
+	constexpr std::int32_t OPENGL_MINOR_VERSION = 1;
 }// namespace taixu::core
 
+enum class status {
+	//Define status enum
+	OK,
+	PERMISSION_FAILED,
+	NO_SUCH_FILE_FAILED
+};
+//namespace taixu: :core
 #endif /* ENGINE_RUNTIME_CORE_BASE_MACRO */
