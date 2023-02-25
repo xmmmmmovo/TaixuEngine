bee148479a3543b1f3cdffedf1eda5b0044b19bd
xmmmmmovo <13256172360@163.com>
2023-02-25 00:05:59 +0000

    修复路径问题+zero warning

--- engine/editor/ui/components/file_component.hpp
+++ engine/editor/ui/components/file_component.hpp
@@ -5,29 +5,33 @@
 #ifndef TAIXUENGINE_FILE_COMPONENT_HPP
 #define TAIXUENGINE_FILE_COMPONENT_HPP
 
+#include <spdlog/spdlog.h>
+
 #include <filesystem>
 #include <string>
 #include <vector>
 
-#include<spdlog/spdlog.h>
 #include "interface/component.hpp"
 
 namespace taixu::editor {
-static const std::filesystem::path s_AssetPath = "E:/TP/TaixuEngine/engine/assets";
 class FileComponent : public IUIComponent {
 private:
     std::vector<std::string_view> file_tree;
     static ImGuiWindowFlags constexpr window_flags =
             ImGuiWindowFlags_HorizontalScrollbar;
 
-    std::filesystem::path m_CurrentDirectory = s_AssetPath;
+    std::filesystem::path root_path{"."};
+    std::filesystem::path current_path{root_path};
 
 public:
     void init() override {}
     void update() override {
-        { 
+        {
 
-            ImGui::BeginChild("FileTree",ImVec2(ImGui::GetWindowContentRegionWidth() * 0.2f, 0.f),false, window_flags);
+            ImGui::BeginChild(
+                    "FileTree",
+                    ImVec2(ImGui::GetWindowContentRegionWidth() * 0.2f, 0.f),
+                    false, window_flags);
             for (int i = 0; i < 100; i++)
                 ImGui::Text("%04d: scrollable region", i);
             ImGui::EndChild();
@@ -38,10 +42,10 @@ public:
         // Child 2: rounded border
         {
             ImGui::BeginChild("ChildR", ImVec2(0, 260), false, window_flags);
-            
-            if (m_CurrentDirectory != std::filesystem::path(s_AssetPath)) {
+
+            if (current_path != root_path) {
                 if (ImGui::Button("<-")) {
-                    m_CurrentDirectory = m_CurrentDirectory.parent_path();
+                    current_path = current_path.parent_path();
                 }
             }
 
@@ -53,22 +57,25 @@ public:
             int   columnCount = (int) (panelWidth / cellSize);
             if (columnCount < 1) columnCount = 1;
 
-            if (ImGui::BeginTable("split", columnCount,ImGuiTableFlags_Resizable |ImGuiTableFlags_NoSavedSettings)) 
-            {              
-                for (auto& directoryEntry :std::filesystem::directory_iterator(m_CurrentDirectory)) 
-                {
+            if (ImGui::BeginTable("split", columnCount,
+                                  ImGuiTableFlags_Resizable |
+                                          ImGuiTableFlags_NoSavedSettings)) {
+                for (auto& directoryEntry :
+                     std::filesystem::directory_iterator(current_path)) {
                     const auto& path = directoryEntry.path();
-                    auto        relativePath = std::filesystem::relative(path, s_AssetPath);
-                    std::string filenameString = relativePath.filename().string();
-                    
+                    auto        relativePath =
+                            std::filesystem::relative(path, root_path);
+                    std::string filenameString =
+                            relativePath.filename().string();
+
                     ImGui::Button(filenameString.c_str(),
                                   {thumbnailSize, thumbnailSize});
-                    if (ImGui::IsItemHovered() &&ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left))
-                    {
+                    if (ImGui::IsItemHovered() &&
+                        ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left)) {
                         if (directoryEntry.is_directory())
-                            m_CurrentDirectory /= path.filename();
+                            current_path /= path.filename();
                     }
-                    ImGui::TextWrapped(filenameString.c_str());
+                    ImGui::TextUnformatted(filenameString.c_str());
                     ImGui::TableNextColumn();
                 }
                 ImGui::EndTable();
@@ -80,25 +87,6 @@ public:
             ImGui::SliderFloat("Padding", &padding, 0, 32);
             ImGui::EndChild();
         }
-        /*ImGui::Begin("Content Browser");
-        static float padding       = 16.0f;
-        static float thumbnailSize = 128.0f;
-        float        cellSize      = thumbnailSize + padding;
-
-        float panelWidth  = ImGui::GetContentRegionAvail().x;
-        int   columnCount = (int) (panelWidth / cellSize);
-        if (columnCount < 1) columnCount = 1;
-
-        ImGui::Columns(columnCount, 0, false);
-        ImGui::Button("chennuo", {thumbnailSize, thumbnailSize});
-        ImGui::Text("chennuo");
-
-        ImGui::Columns(1);
-
-        ImGui::SliderFloat("Thumbnail Size", &thumbnailSize, 16, 512);
-        ImGui::SliderFloat("Padding", &padding, 0, 32);
-
-        ImGui::End;*/
     }
 };
 }// namespace taixu::editor
