3ff76e6304b5a0f10e57525c0b2675ac624a867a
lmdyhm <1227868022@qq.com>
2023-03-03 13:17:48 +0000

    test

--- tests/unit_tests/assets_manager_tests.cpp
+++ tests/unit_tests/assets_manager_tests.cpp
@@ -4,9 +4,13 @@
 #include <catch2/catch_test_macros.hpp>
 
 #include "resource/asset_manager.hpp"
+#include "graphics/render/vertex_array.hpp"
+#include "platform/opengl/OGLVertexArray.hpp"
 
-TEST_CASE("asset_manager") {
+TEST_CASE("noncopyable") {
     //    taixu::Asset_Manager manager{};
     //    manager.loadAsset("assets/asset.json");
     //    manager.writeAsset();
+    //OGLVertexArray o;
+
 }
