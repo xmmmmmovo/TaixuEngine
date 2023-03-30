a156bb2b27a55028a57f43c8527a87639e270be9
xmmmmmovo <xmmmmmovo@users.noreply.github.com>
2023-03-30 00:33:50 +0000

    deploy: b29a73f434acef73fbdd81f5e8ce66a69b55a190

--- classtaixu_1_1editor_1_1Application.js
+++ classtaixu_1_1editor_1_1Application.js
@@ -1,8 +1,8 @@
 var classtaixu_1_1editor_1_1Application =
 [
     [ "destroy", "classtaixu_1_1editor_1_1Application.html#aada48b71c29327bd6c6de0fe8c7ae391", null ],
-    [ "initApplicationArgs", "classtaixu_1_1editor_1_1Application.html#aa7b5cc55d88f21c69bb2acb7caf9cddc", null ],
-    [ "initialize", "classtaixu_1_1editor_1_1Application.html#a4ad6e6934e85d50df1126f12532fdeb4", null ],
+    [ "initApplicationArgs", "classtaixu_1_1editor_1_1Application.html#a44f043b15be79cc53822e7023799ad60", null ],
+    [ "initialize", "classtaixu_1_1editor_1_1Application.html#a7bdd393185269a3bb9c3afe808108ad9", null ],
     [ "run", "classtaixu_1_1editor_1_1Application.html#a5b244b17cb7895f24c21ae3594e7df82", null ],
     [ "PublicSingleton< Application >", "classtaixu_1_1editor_1_1Application.html#a5e4fca70495dc40fe0996c3bf0919c87", null ],
     [ "_engine_ptr", "classtaixu_1_1editor_1_1Application.html#a2e268c4a28213825b609f6fe6f309b65", null ],
