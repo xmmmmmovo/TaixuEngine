1e7f222b77b06bcd01679ebcc730b1872c612ccc
xmmmmmovo <ma_fivezha@163.com>
2024-01-05 01:07:19 +0800

    更新HLSL自动编译

---
+++ engine/shaders/hlsl/dx11/forward_lighting.vs.hlsl
@@ -0,0 +1,10 @@
+#include "lighting.hlsli"
+
+// 顶点着色器
+VertexOut forward_lighting_vs_main(VertexIn vIn)
+{
+    VertexOut vOut;
+    vOut.posH = float4(vIn.pos, 1.0f);
+    vOut.color = vIn.color; // 这里alpha通道的值默认为1.0
+    return vOut;
+}
