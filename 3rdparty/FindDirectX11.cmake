0f3748455c2efd5b727dea008a1f8a21ed1700e7
xmmmmmovo <ma_fivezha@163.com>
2024-01-14 00:45:13 +0800

    更新dx11和vulkan cmake，更新CPM版本

--- 3rdparty/FindDirectX11.cmake
+++ 3rdparty/FindDirectX11.cmake
@@ -1,1 +1,2 @@
-set(DX11_LIB d3d11.lib dxgi.lib dxguid.lib D3DCompiler.lib d2d1.lib dwrite.lib winmm.lib)
+# d3d11.lib dxgi.lib dxguid.lib D3DCompiler.lib d2d1.lib dwrite.lib winmm.lib
+set(DX11_LIB d3d11.lib dxgi.lib dxguid.lib d2d1.lib)
--- 3rdparty/FindVulkanLib.cmake
+++ 3rdparty/FindVulkanLib.cmake
@@ -2,22 +2,24 @@
 find_package(Vulkan REQUIRED)
 
 # TODO: test whether it works that imported by CPM
-if (WIN32)
-    set(VOLK_STATIC_DEFINES VK_USE_PLATFORM_WIN32_KHR)
-elseif (UNIX)
-    if (APPLE)
-        set(VOLK_STATIC_DEFINES VK_USE_PLATFORM_MACOS_MVK)
-    else ()
-        set(VOLK_STATIC_DEFINES VK_USE_PLATFORM_WIN32_KHR)
-    endif ()
-endif ()
+set(VOLK_STATIC_DEFINES_OPT "")
+if(ANDROID)
+    set(VOLK_STATIC_DEFINES_OPT VK_USE_PLATFORM_ANDROID_KHR)
+elseif(X11_FOUND)
+    set(VOLK_STATIC_DEFINES_OPT VK_USE_PLATFORM_XLIB_KHR)
+elseif(WIN32)
+    set(VOLK_STATIC_DEFINES_OPT VK_USE_PLATFORM_WIN32_KHR)
+elseif(APPLE)
+    set(VOLK_STATIC_DEFINES_OPT VK_USE_PLATFORM_MACOS_MVK)
+endif()
 
 # Volk
 CPMAddPackage(
         NAME volk
         GITHUB_REPOSITORY zeux/volk
         GIT_TAG vulkan-sdk-1.3.268.0
-        GIT_SHALLOW TRUE
+        OPTIONS
+        "VOLK_STATIC_DEFINES ${VOLK_STATIC_DEFINES_OPT}"
 )
 
 CPMAddPackage(
