b0189d195155d60d70f77a596ed955e89188f839
xmmmmmovo <ma_fivezha@163.com>
2024-01-14 00:44:43 +0800

    更新windows.h，增加PCH

--- engine/precompile/pch_runtime.hpp
+++ engine/precompile/pch_runtime.hpp
@@ -5,4 +5,50 @@
 #ifndef ENGINE_PRECOMPILE_PCH_RUNTIME_0A0D878DBB714C9499D420BF61F1C4D8
 #define ENGINE_PRECOMPILE_PCH_RUNTIME_0A0D878DBB714C9499D420BF61F1C4D8
 
+
+#define NOGDICAPMASKS
+#define NOVIRTUALKEYCODES
+#define NOWINSTYLES
+#define NOSYSMETRICS
+#define NOMENUS
+#define NOICONS
+#define NOKEYSTATES
+#define NOSYSCOMMANDS
+#define NORASTEROPS
+#define NOSHOWWINDOW
+#define OEMRESOURCE
+#define NOATOM
+#define NOCLIPBOARD
+#define NOCOLOR
+#define NOCTLMGR
+#define NODRAWTEXT
+#define NOGDI
+#define NOKERNEL
+// #define NOUSER
+// #define NONLS
+#define NOMB
+#define NOMEMMGR
+#define NOMETAFILE
+// #define NOMSG
+#define NOOPENFILE
+#define NOSCROLL
+#define NOSERVICE
+
+#define NOMINMAX
+
+#include <magic_enum.hpp>
+
+#include <simdjson/simdjson.h>
+
+#include <Jolt/Jolt.h>
+
+#include <refl.hpp>
+
+#include <glm/glm.hpp>
+
+#include <fmt/format.h>
+#include <spdlog/spdlog.h>
+
+#include <argparse/argparse.hpp>
+
 #endif// ENGINE_PRECOMPILE_PCH_RUNTIME_0A0D878DBB714C9499D420BF61F1C4D8
