6fe507fd9f7e0b7ed04e1e528c64ababcd2347b8
xmmmmmovo <ma_fivezha@163.com>
2024-01-14 00:46:44 +0800

    fix: lint fix in imgui_surface.hpp

--- engine/src/runtime/source/gameplay/gui/imgui_surface.cpp
+++ engine/src/runtime/source/gameplay/gui/imgui_surface.cpp
@@ -8,6 +8,7 @@
 #include "ImGuizmo.h"
 #include "imgui.h"
 #include "imgui_internal.h"
+
 #include <common/base/macro.hpp>
 
 namespace taixu::editor {
@@ -26,13 +27,14 @@ void ImguiLayers::init() {
     io.FontDefault = font;
 
     // merge in icons from Font Awesome
-    static const ImWchar kIconsRanges[] = {ICON_MIN_FA, ICON_MAX_16_FA, 0};
-    ImFontConfig         icons_config;
+    static constexpr std::array<ImWchar, 3> K_ICONS_RANGES{ICON_MIN_FA,
+                                                           ICON_MAX_16_FA, 0};
+    static constexpr float                  FONT_SIZE{16.0f};
+    ImFontConfig                            icons_config;
     icons_config.MergeMode  = true;
     icons_config.PixelSnapH = true;
-    io.Fonts->AddFontFromFileTTF("res/fonts/fa-solid-900.ttf", 16.0f,
-                                 &icons_config, kIconsRanges);
-    // use FONT_ICON_FILE_NAME_FAR if you want regular instead of solid
+    io.Fonts->AddFontFromFileTTF("res/fonts/fa-solid-900.ttf", FONT_SIZE,
+                                 &icons_config, K_ICONS_RANGES.data());
 
     io.ConfigFlags |=
             ImGuiConfigFlags_NavEnableKeyboard;// Enable Keyboard Controls
@@ -45,13 +47,13 @@ void ImguiLayers::init() {
     ImGui::StyleColorsDark();
 
     // cherry colors, 3 intensities
-#define HI(v) ImVec4(0.502f, 0.075f, 0.256f, v)
-#define MED(v) ImVec4(0.455f, 0.198f, 0.301f, v)
-#define LOW(v) ImVec4(0.232f, 0.201f, 0.271f, v)
+#define HI_COL(v) ImVec4(0.502f, 0.075f, 0.256f, v)
+#define MED_COL(v) ImVec4(0.455f, 0.198f, 0.301f, v)
+#define LOW_COL(v) ImVec4(0.232f, 0.201f, 0.271f, v)
 // backgrounds
-#define BG(v) ImVec4(0.200f, 0.220f, 0.270f, v)
+#define BG_COL(v) ImVec4(0.200f, 0.220f, 0.270f, v)
 // text
-#define TEXT(v) ImVec4(0.860f, 0.930f, 0.890f, v)
+#define TEXT_COL(v) ImVec4(0.860f, 0.930f, 0.890f, v)
 
     auto& style = ImGui::GetStyle();
 
@@ -61,46 +63,46 @@ void ImguiLayers::init() {
     style.Colors[ImGuiCol_TabUnfocused] = ImVec4{0.15f, 0.15f, 0.15f, 1.0f};
     style.Colors[ImGuiCol_TabUnfocusedActive] = ImVec4{0.2f, 0.2f, 0.2f, 1.0f};
 
-    style.Colors[ImGuiCol_Text]           = TEXT(0.78f);
-    style.Colors[ImGuiCol_TextDisabled]   = TEXT(0.28f);
+    style.Colors[ImGuiCol_Text]           = TEXT_COL(0.78f);
+    style.Colors[ImGuiCol_TextDisabled]   = TEXT_COL(0.28f);
     style.Colors[ImGuiCol_WindowBg]       = ImVec4(0.13f, 0.14f, 0.17f, 1.00f);
-    style.Colors[ImGuiCol_ChildBg]        = BG(0.58f);
-    style.Colors[ImGuiCol_PopupBg]        = BG(0.9f);
+    style.Colors[ImGuiCol_ChildBg]        = BG_COL(0.58f);
+    style.Colors[ImGuiCol_PopupBg]        = BG_COL(0.9f);
     style.Colors[ImGuiCol_Border]         = ImVec4(0.31f, 0.31f, 1.00f, 0.00f);
     style.Colors[ImGuiCol_BorderShadow]   = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
-    style.Colors[ImGuiCol_FrameBg]        = BG(1.00f);
-    style.Colors[ImGuiCol_FrameBgHovered] = MED(0.78f);
-    style.Colors[ImGuiCol_FrameBgActive]  = MED(1.00f);
-    style.Colors[ImGuiCol_TitleBg]        = LOW(1.00f);
-    style.Colors[ImGuiCol_TitleBgActive]  = HI(1.00f);
-    style.Colors[ImGuiCol_TitleBgCollapsed] = BG(0.75f);
-    style.Colors[ImGuiCol_MenuBarBg]        = BG(0.47f);
-    style.Colors[ImGuiCol_ScrollbarBg]      = BG(1.00f);
+    style.Colors[ImGuiCol_FrameBg]        = BG_COL(1.00f);
+    style.Colors[ImGuiCol_FrameBgHovered] = MED_COL(0.78f);
+    style.Colors[ImGuiCol_FrameBgActive]  = MED_COL(1.00f);
+    style.Colors[ImGuiCol_TitleBg]        = LOW_COL(1.00f);
+    style.Colors[ImGuiCol_TitleBgActive]  = HI_COL(1.00f);
+    style.Colors[ImGuiCol_TitleBgCollapsed] = BG_COL(0.75f);
+    style.Colors[ImGuiCol_MenuBarBg]        = BG_COL(0.47f);
+    style.Colors[ImGuiCol_ScrollbarBg]      = BG_COL(1.00f);
     style.Colors[ImGuiCol_ScrollbarGrab] = ImVec4(0.09f, 0.15f, 0.16f, 1.00f);
-    style.Colors[ImGuiCol_ScrollbarGrabHovered] = MED(0.78f);
-    style.Colors[ImGuiCol_ScrollbarGrabActive]  = MED(1.00f);
+    style.Colors[ImGuiCol_ScrollbarGrabHovered] = MED_COL(0.78f);
+    style.Colors[ImGuiCol_ScrollbarGrabActive]  = MED_COL(1.00f);
     style.Colors[ImGuiCol_CheckMark]  = ImVec4(0.71f, 0.22f, 0.27f, 1.00f);
     style.Colors[ImGuiCol_SliderGrab] = ImVec4(0.47f, 0.77f, 0.83f, 0.14f);
     style.Colors[ImGuiCol_SliderGrabActive] =
             ImVec4(0.71f, 0.22f, 0.27f, 1.00f);
     style.Colors[ImGuiCol_Button]        = ImVec4(0.47f, 0.77f, 0.83f, 0.14f);
-    style.Colors[ImGuiCol_ButtonHovered] = MED(0.86f);
-    style.Colors[ImGuiCol_ButtonActive]  = MED(1.00f);
-    style.Colors[ImGuiCol_Header]        = MED(0.76f);
-    style.Colors[ImGuiCol_HeaderHovered] = MED(0.86f);
-    style.Colors[ImGuiCol_HeaderActive]  = HI(1.00f);
+    style.Colors[ImGuiCol_ButtonHovered] = MED_COL(0.86f);
+    style.Colors[ImGuiCol_ButtonActive]  = MED_COL(1.00f);
+    style.Colors[ImGuiCol_Header]        = MED_COL(0.76f);
+    style.Colors[ImGuiCol_HeaderHovered] = MED_COL(0.86f);
+    style.Colors[ImGuiCol_HeaderActive]  = HI_COL(1.00f);
     style.Colors[ImGuiCol_Separator]     = ImVec4(0.14f, 0.16f, 0.19f, 1.00f);
-    style.Colors[ImGuiCol_SeparatorHovered] = MED(0.78f);
-    style.Colors[ImGuiCol_SeparatorActive]  = MED(1.00f);
+    style.Colors[ImGuiCol_SeparatorHovered] = MED_COL(0.78f);
+    style.Colors[ImGuiCol_SeparatorActive]  = MED_COL(1.00f);
     style.Colors[ImGuiCol_ResizeGrip] = ImVec4(0.47f, 0.77f, 0.83f, 0.04f);
-    style.Colors[ImGuiCol_ResizeGripHovered]    = MED(0.78f);
-    style.Colors[ImGuiCol_ResizeGripActive]     = MED(1.00f);
-    style.Colors[ImGuiCol_PlotLines]            = TEXT(0.63f);
-    style.Colors[ImGuiCol_PlotLinesHovered]     = MED(1.00f);
-    style.Colors[ImGuiCol_PlotHistogram]        = TEXT(0.63f);
-    style.Colors[ImGuiCol_PlotHistogramHovered] = MED(1.00f);
-    style.Colors[ImGuiCol_TextSelectedBg]       = MED(0.43f);
-    style.Colors[ImGuiCol_ModalWindowDimBg]     = BG(0.73f);
+    style.Colors[ImGuiCol_ResizeGripHovered]    = MED_COL(0.78f);
+    style.Colors[ImGuiCol_ResizeGripActive]     = MED_COL(1.00f);
+    style.Colors[ImGuiCol_PlotLines]            = TEXT_COL(0.63f);
+    style.Colors[ImGuiCol_PlotLinesHovered]     = MED_COL(1.00f);
+    style.Colors[ImGuiCol_PlotHistogram]        = TEXT_COL(0.63f);
+    style.Colors[ImGuiCol_PlotHistogramHovered] = MED_COL(1.00f);
+    style.Colors[ImGuiCol_TextSelectedBg]       = MED_COL(0.43f);
+    style.Colors[ImGuiCol_ModalWindowDimBg]     = BG_COL(0.73f);
 
     style.WindowPadding     = ImVec2(6, 4);
     style.WindowRounding    = 0.0f;
@@ -129,7 +131,7 @@ void ImguiLayers::preUpdate() {
     // Start the Dear ImGui frame
     ImGui::NewFrame();
 
-    ImGuiViewport* viewport = ImGui::GetMainViewport();
+    const ImGuiViewport* viewport = ImGui::GetMainViewport();
     ImGui::SetNextWindowPos(viewport->Pos);
     ImGui::SetNextWindowSize(viewport->Size);
     ImGui::SetNextWindowViewport(viewport->ID);
