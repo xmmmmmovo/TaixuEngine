0f1ef482f3a5d0ed526f280df69af5a997929bd1
xmmmmmovo <13256172360@163.com>
2024-07-17 23:20:03 +0800

    fix(macos): 修复mac上编译问题

--- engine/src/runtime/source/management/gpu/shaders/shader_manager.cpp
+++ engine/src/runtime/source/management/gpu/shaders/shader_manager.cpp
@@ -21,14 +21,14 @@ std::shared_ptr<TXShaderModule> TXShaderModuleManager::createCustomShaderModule(
 
 std::shared_ptr<TXShaderModule> TXShaderModuleManager::getBuiltinShaderModule(
         EnumTXBuiltinShader builtin_shader) const {
-    if (_builtin_modules[static_cast<size_t>(builtin_shader)] == nullptr) {
+    if (builtin_modules[static_cast<size_t>(builtin_shader)] == nullptr) {
         std::shared_ptr<TXShaderModule> module = createShaderModuleInner(
-                _builtin_shader_create_infos[static_cast<size_t>(
+                builtin_shader_create_infos[static_cast<size_t>(
                         builtin_shader)]);
-        _builtin_modules[static_cast<size_t>(builtin_shader)] = module;
+        builtin_modules[static_cast<size_t>(builtin_shader)] = module;
         return module;
     }
-    return _builtin_modules[static_cast<size_t>(builtin_shader)]
+    return builtin_modules[static_cast<size_t>(builtin_shader)]
             ->shared_from_this();
 }
 
