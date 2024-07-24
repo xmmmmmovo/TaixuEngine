068f81683f06addc54a7614137afd7f627573a6e
xmmmmmovo <13256172360@163.com>
2024-07-24 23:28:42 +0800

    refactor(cmake): 迁移到CPP23

--- 3rdparty/FindImgui.cmake
+++ 3rdparty/FindImgui.cmake
@@ -1,51 +1,52 @@
 # imgui
 CPMAddPackage(
-        NAME imgui
-        GIT_REPOSITORY https://github.com/ocornut/imgui
-        GIT_TAG 8048b52498a9bf2a9f87b080d43b0bfd7a5d51d8
-        DOWNLOAD_ONLY YES
+    NAME imgui
+    GIT_REPOSITORY https://github.com/ocornut/imgui
+    GIT_TAG 8048b52498a9bf2a9f87b080d43b0bfd7a5d51d8
+    DOWNLOAD_ONLY YES
 )
 
-if (imgui_ADDED)
+if(imgui_ADDED)
     file(GLOB imgui_sources CONFIGURE_DEPENDS ${imgui_SOURCE_DIR}/*.cpp)
 
     file(GLOB imgui_impl CONFIGURE_DEPENDS
-            ${imgui_SOURCE_DIR}/backends/imgui_impl_glfw.cpp
-            ${imgui_SOURCE_DIR}/backends/imgui_impl_glfw.h
+        ${imgui_SOURCE_DIR}/backends/imgui_impl_glfw.cpp
+        ${imgui_SOURCE_DIR}/backends/imgui_impl_glfw.h
     )
 
-    if (USE_VULKAN)
+    if(USE_VULKAN)
         # append file to imgui_impl
         list(APPEND imgui_impl ${imgui_SOURCE_DIR}/backends/imgui_impl_vulkan.cpp)
         list(APPEND imgui_impl ${imgui_SOURCE_DIR}/backends/imgui_impl_vulkan.h)
-    endif ()
+    endif()
 
-    if (USE_DX11)
+    if(USE_DX11)
         # append file to imgui_impl
         list(APPEND imgui_impl ${imgui_SOURCE_DIR}/backends/imgui_impl_dx11.cpp)
         list(APPEND imgui_impl ${imgui_SOURCE_DIR}/backends/imgui_impl_dx11.h)
-    endif ()
+    endif()
 
     set(ADDON_PATH ${PROJECT_SOURCE_DIR}/3rdparty/imgui_addons)
 
     file(GLOB_RECURSE imgui_ext_sources CONFIGURE_DEPENDS ${ADDON_PATH}/*.cpp)
 
     add_library(imgui STATIC ${imgui_sources} ${imgui_impl} ${imgui_ext_sources})
+    set_target_properties(imgui PROPERTIES CXX_STANDARD 23)
 
     target_include_directories(imgui PUBLIC $<BUILD_INTERFACE:${imgui_SOURCE_DIR}>)
     target_include_directories(imgui PUBLIC $<BUILD_INTERFACE:${ADDON_PATH}>)
 
     target_link_libraries(imgui PUBLIC freetype glfw)
 
-    if (USE_VULKAN)
+    if(USE_VULKAN)
         target_include_directories(imgui PUBLIC $<BUILD_INTERFACE:${Vulkan_INCLUDE_DIR}>)
         target_link_libraries(imgui PUBLIC Vulkan::Vulkan)
-    endif ()
+    endif()
 
-    if (USE_DX11)
+    if(USE_DX11)
         target_link_libraries(imgui PUBLIC ${DX11_LIB})
-    endif ()
+    endif()
 
     set_target_properties(imgui PROPERTIES LINKER_LANGUAGE CXX)
     set_target_properties(imgui PROPERTIES FOLDER 3rdparty)
-endif ()
+endif()
--- engine/precompile/pch_vulkan.hpp
+++ engine/precompile/pch_vulkan.hpp
@@ -11,6 +11,10 @@
 
 #include <vk_mem_alloc.h>
 
+#define VULKAN_HPP_NO_EXCEPTIONS
+#define VULKAN_HPP_RAII_NO_EXCEPTIONS
+
+#include <vulkan/vulkan_hpp_macros.hpp>
 #include <vulkan/vulkan_raii.hpp>
 
 #endif// ENGINE_PRECOMPILE_PCH_VULKAN_CD66C63DA4B64D3BBA17477BBEAABFC2
